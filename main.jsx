ee169ee70d88984e9739b80c6ea9da75904ac1d4
vaabtour-ship-it <vaabtour@gmail.com>
2026-06-24 11:36:37 +0200

    Fix: activation des vraies pages de suivi

--- main.jsx
+++ main.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx' 
+import MarqueBResult from './MarqueBResult.jsx' // On reconnecte ta page Marque B
+import TrackingResult from './TrackingResult.jsx' // On reconnecte ta page de suivi standard
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Ta page principale qui contient ton tracker */}
+        {/* Page d'accueil (Le moteur de recherche) */}
         <Route path="/" element={<App />} />
         
-        {/* Redirection automatique vers l'accueil si l'utilisateur va ailleurs */}
-        <Route path="*" element={<App />} />
+        {/* Page pour les colis Marque B (numéros contenant 0731 ou 07) */}
+        <Route path="/marque-b" element={<MarqueBResult />} />
+        
+        {/* Page pour le suivi normal (numéros contenant 3107 ou 31) */}
+        <Route path="/suivi" element={<TrackingResult />} />
+
+        {/* Route dynamique si tu tapes directement le numéro dans l'URL (ex: /FR123456) */}
+        <Route path="/:id" element={<TrackingResult />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
